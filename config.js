d8447d9b6d35234af0aee87db82198201a3ca299
NickWeaver02 <anweaver02@gmail.com>
2025-08-03 14:39:32 -0700

    Here we go

---
+++ config.js
@@ -0,0 +1,9 @@
+
+window.APP_CONFIG = {
+  REDIRECT_URI: 'https://prettyquizzer.github.io/Quiz/',
+  CLIENT_ID: '28kigssmjf2mdhjn6iavt1ua7e',
+  AUTHORITY: 'https://cognito-idp.us-east-2.amazonaws.com/us-east-2_acveeSnQD',
+  SCOPE: 'openid email profile',
+  POST_LOGOUT_URI: "https://prettyquizzer.github.io/Quiz/",
+  AWS_DOMAIN: 'https://us-east-2_acveeSnQD.auth.us-east-2.amazoncognito.com'
+};
